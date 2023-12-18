99fc722ed246193409354219b80db6124ecbfb8e
mnonnenm <marcel.nonnenm@gmail.com>
2023-12-18 19:51:49 +0000

    adding basic implementation of spherical K-means

--- vMFne/bregman_clustering.py
+++ vMFne/bregman_clustering.py
@@ -34,7 +34,7 @@ def softBregmanClustering_vMF(X, K, max_iter=100, w_init=None, μs_init=None, ve
         #μs_init[K-1] = X[idx[(K-1)*N//K:]].mean(axis=0) # last partition can be larger
         #μs_init = 0.9 * μs_init # pull means towards origin (make clusters broader)
         μs_init = np.random.normal(size=(K,D))
-        μs_init = μs_init / (100. * np.linalg.norm(μs_init,axis=-1).reshape(-1,1))
+        μs_init = μs_init / (1e6 * np.linalg.norm(μs_init,axis=-1).reshape(-1,1))
 
     μs = μs_init 
 
@@ -59,3 +59,44 @@ def softBregmanClustering_vMF(X, K, max_iter=100, w_init=None, μs_init=None, ve
             print('||μs||:', np.linalg.norm(μs,axis=-1))
 
     return μs, w, LL[:ii+1]
+
+def spherical_kmeans(X, K, max_iter=100, w_init=None, μs_init=None, verbose=False):
+
+    N,D = X.shape
+
+    w = np.ones(K)/K if w_init is None else w_init
+    assert np.all(w >= 0.)
+    assert np.allclose(w.sum(), 1.)
+
+    # initialize clusters on means of random partitioning
+    if μs_init is None:
+        idx = np.random.permutation(N)
+        μs_init = np.zeros((K,D))
+        for k in range(K-1):
+            μs_init[k] = X[idx[k*N//K : (k+1)*N//K]].mean(axis=0)
+        μs_init[K-1] = X[idx[(K-1)*N//K:]].mean(axis=0) # last partition can be larger
+
+    μs = μs_init
+    μs = μs / np.linalg.norm(μs,axis=-1).reshape(K,1)
+
+    if verbose:
+        print('initial w:', w)
+        print('inital ||μs||:', np.linalg.norm(μs,axis=-1))
+
+    for ii in range(max_iter):
+
+        # E-step: - compute cluster assignments via cosine similarity
+        c = np.argmax(X.dot(μs.T),axis=-1) # X*μs' is N x K, c is N-dim.
+
+        # M-step:
+        w = np.array([np.mean(c==k) for k in range(K)])
+        μs = np.stack([X[c==k].mean(axis=0) for k in range(K)],axis=0)
+        μs_norm = np.linalg.norm(μs,axis=-1).reshape(K,1)
+        μs_norm[μs_norm==0] = 1.
+        μs = μs / μs_norm
+
+        if verbose:
+            print(' #' + str(ii+1) + '/' + str(max_iter))
+            print('w:', w)
+
+    return μs, w, c
