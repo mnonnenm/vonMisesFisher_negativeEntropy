c55a5027dea61392cba4c9da11a9310de3e44bd5
Marcel Nonnenmarcher <mnonnenmacher@u335b.id.gatsby.ucl.ac.uk>
2024-01-08 20:47:30 +0000

    adding hard-assignment versions of algorithms, refactoring old code

--- vMFne/bregman_clustering.py
+++ vMFne/bregman_clustering.py
@@ -8,14 +8,52 @@ def vMF_loglikelihood_Ψ(x,μ,D=2,Ψ0=None):
     # μ : D-dim. vector or K-D matrix
     return - DBregΨ(x,μ,D=D,treat_x_constant=True,Ψ0=Ψ0) # N x K
 
+
+def log_joint_vMF_mixture_Ψ(X,w,μs,Ψ0=0.):
+
+    K,D = μs.shape
+    logpxh = np.log(w).reshape(1,K) + vMF_loglikelihood_Ψ(X,μs,D,Ψ0=Ψ0)
+
+    return logpxh
+
+
 def posterior_marginal_vMF_mixture_Ψ(X,w,μs,Ψ0=0.):
 
-    N,K,D = X.shape[0], μs.shape[0], X.shape[1]
-    LL_k = vMF_loglikelihood_Ψ(X,μs,D,Ψ0=Ψ0) # N x K
-    logpxh = np.log(w.reshape(1,K)) + LL_k   # N x K
+    N = X.shape[0]
+    logpxh = log_joint_vMF_mixture_Ψ(X,w,μs,Ψ0=Ψ0)
     logpx = scipy.special.logsumexp(logpxh,axis=1).reshape(N,1)
 
     return np.exp(logpxh - logpx), logpx
+
+
+def em_M_step_Ψ(X, post, μ_norm_max=0.99, tie_norms=False):
+
+    K = post.shape[-1]
+
+    w = post.mean(axis=0)
+    μs = post.T.dot(X) / post.sum(axis=0).reshape(K,1)
+    μs_norm = np.linalg.norm(μs,axis=-1)
+    if np.any(μs_norm>μ_norm_max):
+        idx = μs_norm>μ_norm_max
+        μs[idx,:] = μ_norm_max * μs[idx,:] / μs_norm[idx].reshape(-1,1)
+    if tie_norms:
+        μs = μs / μs_norm.reshape(K,1) * μs_norm.mean()
+
+    return w, μs
+
+
+def init_EM_Ψ(X, K):
+
+    N, D = X.shape
+
+    idx = np.random.permutation(N)
+    μs_init = np.zeros((K,D))
+    for k in range(K-1):
+        μs_init[k] = X[idx[k*N//K : (k+1)*N//K]].mean(axis=0)
+    μs_init[K-1] = X[idx[(K-1)*N//K:]].mean(axis=0) # last partition can be larger
+
+    return μs_init
+
 
 def softBregmanClustering_vMF(X, K, max_iter=100, w_init=None, μs_init=None, verbose=False, Ψ0=[0., 1e-6],
                               tie_norms=False, μ_norm_max=0.99):
@@ -27,13 +65,8 @@ def softBregmanClustering_vMF(X, K, max_iter=100, w_init=None, μs_init=None, ve
     assert np.allclose(w.sum(), 1.)
 
     # initialize clusters on means of random partitioning
-    if μs_init is None:
-        idx = np.random.permutation(N)
-        μs_init = np.zeros((K,D))
-        for k in range(K-1):
-            μs_init[k] = X[idx[k*N//K : (k+1)*N//K]].mean(axis=0)
-        μs_init[K-1] = X[idx[(K-1)*N//K:]].mean(axis=0) # last partition can be larger
-    μs = μs_init 
+    μs = init_EM_Ψ(X, K) if μs_init is None else μs_init
+    assert np.all(np.linalg.norm(μs,axis=-1) <= 1.0)
 
     if verbose:
         print('initial w:', w)
@@ -47,16 +80,7 @@ def softBregmanClustering_vMF(X, K, max_iter=100, w_init=None, μs_init=None, ve
         LL[ii] = logpx.sum()
 
         # M-step:
-        w = post.mean(axis=0)
-        μs = post.T.dot(X) / post.sum(axis=0).reshape(K,1)
-
-        μs_norm = np.linalg.norm(μs,axis=-1)
-        if np.any(μs_norm>μ_norm_max):
-            idx = μs_norm>μ_norm_max
-            μs[idx,:] = μ_norm_max * μs[idx,:] / μs_norm[idx].reshape(-1,1)
-
-        if tie_norms:
-            μs = μs / μs_norm.reshape(-1,1) * μs_norm.mean()
+        w, μs = em_M_step_Ψ(X, post, μ_norm_max=μ_norm_max, tie_norms=tie_norms)
 
         if verbose:
             print(' #' + str(ii+1) + '/' + str(max_iter))
@@ -64,6 +88,45 @@ def softBregmanClustering_vMF(X, K, max_iter=100, w_init=None, μs_init=None, ve
             print('||μs||:', np.linalg.norm(μs,axis=-1))
 
     return μs, w, LL[:ii+1]
+
+
+def hardBregmanClustering_vMF(X, K, max_iter=100, w_init=None, μs_init=None, verbose=False, Ψ0=[0., 1e-6],
+                              tie_norms=False, μ_norm_max=0.99):
+
+    N,D = X.shape
+
+    w = np.ones(K)/K if w_init is None else w_init
+    assert np.all(w >= 0.)
+    assert np.allclose(w.sum(), 1.)
+
+    # initialize clusters on means of random partitioning
+    μs = init_EM_Ψ(X, K) if μs_init is None else μs_init
+    assert np.all(np.linalg.norm(μs,axis=-1) <= 1.0)
+
+    if verbose:
+        print('initial w:', w)
+        print('initial ||μs||:', np.linalg.norm(μs,axis=-1))
+
+    LL = np.zeros(max_iter) # likelihood (up to multiplicative constant)
+    for ii in range(max_iter):
+
+        # E-step: - compute (hardened) cluster responsibilities
+        logpxh = log_joint_vMF_mixture_Ψ(X,w,μs,Ψ0=Ψ0)
+        post = (logpxh >= (np.max(logpxh,axis=1).reshape(-1,1) * np.ones(1,K)))
+
+        logpx = scipy.special.logsumexp(logpxh,axis=1).reshape(N,1)
+        LL[ii] = logpx.sum()
+
+        # M-step:
+        w, μs = em_M_step_Ψ(X, post, μ_norm_max=μ_norm_max, tie_norms=tie_norms)
+
+        if verbose:
+            print(' #' + str(ii+1) + '/' + str(max_iter))
+            print('w:', w)
+            print('||μs||:', np.linalg.norm(μs,axis=-1))
+
+    return μs, w, LL[:ii+1]
+
 
 def spherical_kmeans(X, K, max_iter=100, w_init=None, μs_init=None, verbose=False):
 
@@ -74,19 +137,11 @@ def spherical_kmeans(X, K, max_iter=100, w_init=None, μs_init=None, verbose=Fal
     assert np.allclose(w.sum(), 1.)
 
     # initialize clusters on means of random partitioning
-    if μs_init is None:
-        idx = np.random.permutation(N)
-        μs_init = np.zeros((K,D))
-        for k in range(K-1):
-            μs_init[k] = X[idx[k*N//K : (k+1)*N//K]].mean(axis=0)
-        μs_init[K-1] = X[idx[(K-1)*N//K:]].mean(axis=0) # last partition can be larger
-
-    μs = μs_init
-    μs = μs / np.linalg.norm(μs,axis=-1).reshape(K,1)
+    μs = init_EM_Ψ(X, K) if μs_init is None else μs_init
+    μs = μs / np.linalg.norm(μs,axis=-1).reshape(K,1) # centroids are on sphere surface
 
     if verbose:
         print('initial w:', w)
-        print('initial ||μs||:', np.linalg.norm(μs,axis=-1))
 
     for ii in range(max_iter):
 
--- vMFne/moVMF.py
+++ vMFne/moVMF.py
@@ -3,16 +3,54 @@ from vMFne.negentropy import gradΨ
 from vMFne.logpartition import vMF_loglikelihood_Φ
 import scipy.special
 
+
+def log_joint_vMF_mixture_Φ(X,w,ηs):
+
+    logpxh = np.log(w).reshape(1,ηs.shape[0]) + vMF_loglikelihood_Φ(X,ηs,incl_const=False)
+
+    return logpxh
+
+
 def posterior_marginal_vMF_mixture_Φ(X,w,ηs):
 
-    N,K,D = X.shape[0], ηs.shape[0], X.shape[1]
-    LL_k = vMF_loglikelihood_Φ(X,ηs,incl_const=False)
-    logpxh = np.log(w.reshape(1,K)) + LL_k
-    logpx = scipy.special.logsumexp(logpxh,axis=1).reshape(N,1)
+    logpxh = log_joint_vMF_mixture_Φ(X,w,ηs)
+    logpx = scipy.special.logsumexp(logpxh,axis=1).reshape(X.shape[0],1)
 
     return np.exp(logpxh - logpx), logpx
 
-def moVMF(X, K, max_iter=50, w_init=None, ηs_init=None, verbose=False, κ_max=np.inf, tie_norms=False):
+
+def em_M_step_Φ(X, post, κ_max=np.inf, tie_norms=False):
+
+    D = X.shape[-1]
+
+    w = post.mean(axis=0)
+    nalphas = post.sum(axis=0)
+    mus = post.T.dot(X)
+    mu_norms = np.linalg.norm(mus,axis=1)
+    rbar = mu_norms / nalphas
+    mus = mus / mu_norms.reshape(-1,1)  # unit-norm 'mean parameter' vectors
+    κs = np.minimum(rbar * (D - rbar**2) / (1 - rbar**2), κ_max)
+    if tie_norms:
+        κs = κs.mean() * np.ones_like(κs)
+    ηs = mus * κs.reshape(-1,1)
+
+    return w, ηs
+
+
+def init_EM_Φ(X, K):
+
+    N, D = X.shape
+
+    idx = np.random.permutation(N)
+    μs_init = np.zeros((K,D))
+    for k in range(K-1):
+        μs_init[k] = X[idx[k*N//K : (k+1)*N//K]].mean(axis=0)
+    μs_init[K-1] = X[idx[(K-1)*N//K:]].mean(axis=0) # last partition can be larger
+
+    return gradΨ(μs_init,D=D)
+
+
+def softMoVMF(X, K, max_iter=50, w_init=None, ηs_init=None, verbose=False, κ_max=np.inf, tie_norms=False):
 
     N,D = X.shape
 
@@ -21,15 +59,7 @@ def moVMF(X, K, max_iter=50, w_init=None, ηs_init=None, verbose=False, κ_max=n
     assert np.allclose(w.sum(), 1.)
 
     # initialize clusters on means of random partitioning
-    if ηs_init is None:
-        idx = np.random.permutation(N)
-        μs_init = np.zeros((K,D))
-        for k in range(K-1):
-            μs_init[k] = X[idx[k*N//K : (k+1)*N//K]].mean(axis=0)
-        μs_init[K-1] = X[idx[(K-1)*N//K:]].mean(axis=0) # last partition can be larger
-        μs_init = 0.9 * μs_init # pull means towards origin (make clusters broader) 
-        ηs_init = gradΨ(μs_init,D=D)
-    ηs = ηs_init
+    ηs = init_EM_Φ(X, K) if ηs_init is None else ηs_init
 
     if verbose:
         print('initial w:', w)
@@ -39,25 +69,48 @@ def moVMF(X, K, max_iter=50, w_init=None, ηs_init=None, verbose=False, κ_max=n
     for ii in range(max_iter):
 
         # E-step: - compute cluster responsibilities
-        post, px = posterior_marginal_vMF_mixture_Φ(X=X,w=w,ηs=ηs)
-        LL[ii] = np.log(px).sum()
+        post, logpx = posterior_marginal_vMF_mixture_Φ(X=X,w=w,ηs=ηs)
+        LL[ii] = logpx.sum()
 
         # M-step:
-        w = post.mean(axis=0)
-
-        nalphas = post.sum(axis=0)
-        mus = post.T.dot(X)
-        mu_norms = np.linalg.norm(mus,axis=1)
-        rbar = mu_norms / nalphas
-        mus = mus / mu_norms.reshape(-1,1)  # unit-norm 'mean parameter' vectors
-        κs = np.minimum(rbar * (D - rbar**2) / (1 - rbar**2), κ_max)
-        if tie_norms:
-            κs = κs.mean() * np.ones_like(κs)
-
-        ηs = mus * κs.reshape(-1,1)
-        #μs = mus/ post.sum(axis=0).reshape(K,1)
-        #ηs = gradΨ(μs,D=D)
+        w, ηs = em_M_step_Φ(X, post, κ_max=np.inf, tie_norms=False)
         
+        if verbose:
+            print(' #' + str(ii+1) + '/' + str(max_iter))
+            print('w:', w)
+            print('kappa:', np.linalg.norm(ηs,axis=-1))
+
+    return ηs, w, LL[:ii+1]
+
+
+def hardMoVMF(X, K, max_iter=50, w_init=None, ηs_init=None, verbose=False, κ_max=np.inf, tie_norms=False):
+
+    N,D = X.shape
+
+    w = np.ones(K)/K if w_init is None else w_init
+    assert np.all(w >= 0.)
+    assert np.allclose(w.sum(), 1.)
+
+    # initialize clusters on means of random partitioning        
+    ηs = init_EM_Φ(X, K) if ηs_init is None else ηs_init
+
+    if verbose:
+        print('initial w:', w)
+        print('inital kappa:', np.linalg.norm(ηs,axis=-1))
+
+    LL = np.zeros(max_iter) # likelihood (up to multiplicative constant)
+    for ii in range(max_iter):
+
+        # E-step: - compute (hardened) cluster responsibilities
+        logpxh = log_joint_vMF_mixture_Φ(X=X,w=w,ηs=ηs)
+        post = (logpxh >= (np.max(logpxh,axis=1).reshape(-1,1) * np.ones(1,K)))
+
+        logpx = scipy.special.logsumexp(logpxh,axis=1).reshape(N,1)
+        LL[ii] = logpx.sum()
+
+        # M-step:
+        w, ηs = em_M_step_Φ(X, post, κ_max=κ_max, tie_norms=tie_norms)
+
         if verbose:
             print(' #' + str(ii+1) + '/' + str(max_iter))
             print('w:', w)
